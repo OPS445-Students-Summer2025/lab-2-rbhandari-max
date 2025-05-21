1bd7fa69c939a821ce61320b955d13e60e46063d
rbhandari17 <rbhandari17@myseneca.ca>
2025-05-21 15:29:55 -0400

    file lab2e.py added

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Rojina Bhandrai
+# Author ID: 143292233
+# Date Created: 2025/05/21
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")

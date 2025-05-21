d7f38e24bdcd9c85b0a7d845bc278f5549604512
rbhandari17 <rbhandari17@myseneca.ca>
2025-05-21 15:30:17 -0400

    file lab2f.py added

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Rojina Bhandari
+# Author ID: 143292233
+# Date Created: 2025/05/21
+
+import sys
+
+timer = int(sys.argv[1])  # convert argument to integer
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
